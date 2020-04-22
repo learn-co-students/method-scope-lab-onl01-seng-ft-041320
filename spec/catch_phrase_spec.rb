38fd5035a563536eef7966f5070779fa350ac460
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-22 21:13:29 +0000

    Automatically backed up by Learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 describe "#catch_phrase" do
   it "puts out a catch phrase" do
    catch_phrase = "It's-a me, Mario!"
-    expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
+    xpect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
